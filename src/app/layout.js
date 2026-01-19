67a1f3a1c115cbb47bc453bb41d72d076e8515b5
WebGomezPalacio <luisjaviermtzb@gmail.com>
2026-01-19 13:45:14 -0600

    feat: add citas01

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/authContext";
 
+export const dynamic = "force-dynamic";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
