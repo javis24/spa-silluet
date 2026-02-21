2cecfbf0a7dc08113c8c675b1da33fe14ad20b17
WebGomezPalacio <luisjaviermtzb@gmail.com>
2026-02-20 20:09:30 -0600

    fix: cleanup-remove-3

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 1. Optimización para Vercel
   output: 'standalone',
 
-  // 2. Evita que Next.js intente procesar los binarios de la DB
+
   serverExternalPackages: ['sequelize', 'mysql2'],
 
-  // 3. Solución al "Call retries were exceeded" (limita a un solo proceso de build)
   experimental: {
-    workerThreads: false,
+
     cpus: 1,
+    workerThreads: false,
   },
 
-  // 4. Saltamos validaciones pesadas para agilizar el build
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 
-  // 5. Configuración de Webpack para evitar errores de módulos de Node en el cliente
-  webpack: (config, { isServer }) => {
-    config.module.exprContextCritical = false;
+  turbopack: {}, 
 
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        child_process: false,
-        dns: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
